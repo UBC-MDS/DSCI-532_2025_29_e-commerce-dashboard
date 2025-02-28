d38d1ae945517d2ff922f470c5b996950dd50d21
JENSON Chang <jensonchang@gmail.com>
2025-02-28 14:26:35 -0800

    Connected filtering

--- src/app.py
+++ src/app.py
@@ -7,20 +7,28 @@ import geopandas as gpd
 
 alt.data_transformers.enable('vegafusion')
 
+# Import sales data for dashboard
 def import_data():
 
     # Import and clean sales data
     df = pd.read_csv('data/raw/amazon_sample.zip')
+
+    # Drop last column
     df = df.iloc[:, :-1]  # Drop last column
+
+    # Rename column name and drop na values
     df.rename(columns={'ship-state' : 'state'}, inplace=True)
     df['state'] = df['state'].str.title()
     df['state'].dropna(inplace=True)
+
+    # Convert Date column to datetime
     df["Date"] = pd.to_datetime(df["Date"], format="%m-%d-%y", errors="coerce")
 
-    # Import geojson file for India
-    url = 'https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_1_states_provinces.zip'
-    india = gpd.read_file(url).query("iso_a2 == 'IN'")
-    india.rename(columns={'name' : 'state'}, inplace=True)
+    # Extract year-month for aggregation
+    df["year_month"] = df["Date"].dt.to_period("M").astype(str)
+
+    # Ddd flag for promotions
+    df['is_promotion'] = df['promotion-ids'].notna() # will capture both NA and empty string
 
     # Mapping to rename state names in sales data
     state_mapping = {
@@ -34,9 +42,17 @@ def import_data():
     df['state'] = df['state'].replace(state_mapping)
     df.dropna(inplace=True)
 
-    return df, india
+    return df
 
-# function to format numeric values
+# Import geojson file for India
+def import_geojson():  
+    url = 'https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_1_states_provinces.zip'
+    india = gpd.read_file(url).query("iso_a2 == 'IN'")
+    india.rename(columns={'name' : 'state'}, inplace=True)
+
+    return india
+
+# Function to format numeric values
 def format_large_num(value):
     value = float('{:.3g}'.format(value))
     magnitude = 0
@@ -50,7 +66,8 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 # Data
-df, india = import_data()
+df = import_data()
+india = import_geojson()
 
 status_mapping = {
     'Cancelled': ['Cancelled'],
@@ -60,15 +77,6 @@ status_mapping = {
 	    'Shipped - Picked Up', 'Shipped - Rejected by Buyer',
 	    'Shipped - Returned to Seller', 'Shipped - Returning to Seller'],
 }
-
-# Convert Date column to datetime
-df["Date"] = pd.to_datetime(df["Date"], format="%m-%d-%y", errors="coerce")
-
-# Extract year-month for aggregation
-df["year_month"] = df["Date"].dt.to_period("M").astype(str)
-
-# add flag for promotions
-df['is_promotion'] = df['promotion-ids'].notna() # will capture both NA and empty string
 
 # Extract all unique year-month values from the full dataset
 all_months = df["year_month"].unique()
@@ -82,7 +90,8 @@ df_month_values = (
     df.groupby('year_month').agg({'Amount': 'sum', 'Qty': 'sum'})
     .sort_index()
 )[-2:]
-# create list of the last 2 months and filter for metric
+
+# Create list of the last 2 months and filter for metric
 last2_months = df_month_values.index.to_list()
 df_metric = df[df["year_month"].isin(last2_months)]
 
@@ -97,12 +106,12 @@ else:
 
 # Compute Quantity Sold Change
 qty_mom = df_month_values[['Qty']]
+
 # Ensure we have at least two months of data before computing the percentage change
 if len(qty_mom) > 1:
     quantity_mom_change = (qty_mom.pct_change().iloc[-1].item())
 else:
     quantity_mom_change = 0  # Default to 0% change if not enough data
-
 
 total_revenue_current = revenue_mom.iloc[-1].item()
 total_quantity_current = qty_mom.iloc[-1].item()
@@ -114,11 +123,10 @@ df.loc[:, "order_status_category"] = df["Status"].apply(lambda x: "Completed" if
 monthly_counts = df.groupby("year_month")["order_status_category"].count()
 completed_counts = df[df["order_status_category"] == "Completed"].groupby("year_month")["order_status_category"].count()
 
-# get the completion rate, ensure values sorted by month
+# Get the completion rate, ensure values sorted by month
 completion_rate = (completed_counts / monthly_counts).sort_index() * 100
-
-
 completion_rate_current = completion_rate.iloc[-1]
+
 # Ensure 2+ months worth of data exists before accessing
 if len(completion_rate.index)>1:
     completion_rate_prev = completion_rate.iloc[-2]
@@ -130,7 +138,7 @@ else:
 # Header / Title
 title = dbc.Row(html.H1("Sales Dashboard"), id='title')
 
-# Metrics Cards using computed values
+
 # Metrics Cards using computed values
 metric_1 = dbc.Card(
     dbc.CardBody(
@@ -216,7 +224,6 @@ fulfillment_radio = dbc.Col([
     )
 ], width=3)
 
-
 # Checkbox for order status, use grouped statuses
 status_checkbox = dbc.Col([
     dcc.Checklist(
@@ -226,7 +233,6 @@ status_checkbox = dbc.Col([
         inline=False  # Display vertically
     )
 ])
-
 
 # Filters section (moving slider under "Space for Filters")
 filters = dbc.Col([
@@ -306,6 +312,7 @@ visuals = dbc.Row([
 
 # Layout
 app.layout = dbc.Container([
+    dcc.Store(id="filter_condition", data={}),
     # Title
     dbc.Row([
         dbc.Col(title, width=12)
@@ -326,13 +333,14 @@ app.layout = dbc.Container([
 # Server side callbacks/reactivity
 @app.callback(
     Output("filtered-data", "children"),  # Debugging output
-    #Output("filtered-df-store", "data"),  # Store filtered data for teammates
+    Output("filter_condition", "data"),
     Input("date-slider", "value"),
     Input("promotion-toggle", "value"),
     Input("fulfillment-radio", "value"),
     Input("status-checkbox", "value"),
+    Input("map", "signalData"),
 )
-def update_filtered_data(selected_index, promo_filter, fulfillment_filter, selected_statuses):
+def update_filtered_data(selected_index, promo_filter, fulfillment_filter, selected_statuses, signal_data):
     # Convert slider index to corresponding year-month
     selected_date = month_labels.get(selected_index, None)
 
@@ -340,44 +348,41 @@ def update_filtered_data(selected_index, promo_filter, fulfillment_filter, selec
         return "No selection"
 
     # set end date to proper date
-    filter_end_date = pd.to_datetime(f'{selected_date}-01') + pd.DateOffset(months = 1)
-    filter_condition = '(Date < @filter_end_date)'
+    filter_end_date = pd.to_datetime(f'{selected_date}-01') + pd.DateOffset(months=1)
+    filter_condition = f'(Date < "{filter_end_date}")'
 
     # Apply promotion filter
     if promo_filter:
-        #filter_condition &= (df['is_promotion'])
         filter_condition += ' & (is_promotion == True)'
 
     # Apply fulfillment filter if selection is not Both
     if fulfillment_filter != "Both":  # If not "Both", filter accordingly
-        filter_condition += ' & (Fulfilment == @fulfillment_filter)'
-
+        filter_condition += f' & (Fulfilment == "{fulfillment_filter}")'
 
     # Apply order status filter
     if selected_statuses:
-        #filter_condition &= (df[df["Status"].isin(selected_statuses)])
-        filter_statuses = [item for key, values in status_mapping.items() for item in values if key in selected_statuses ]
-        filter_condition += ' & (Status.isin(@filter_statuses))'
+        filter_statuses = [item for key, values in status_mapping.items() for item in values if key in selected_statuses]
+        filter_statuses_str = ', '.join([f'"{status}"' for status in filter_statuses])
+        filter_condition += f' & (Status in [{filter_statuses_str}])'
 
-    # Store the filtered dataset as JSON (so teammates can use it)
-    # print(filter_condition)
-    filtered_df = df.query(filter_condition)    
-    filtered_data_json = filtered_df.to_json(orient="split")
+    if signal_data and 'state' in signal_data['selected_states']:
+        state = signal_data['selected_states']['state'][0]
+        filter_condition += f' & (state == "{state}")'
 
-    return f"Showing {len(filtered_df):,.0f} records up to {selected_date}."#, filtered_data_json
+    # Store the filtered dataset
+    filtered_df = df.query(filter_condition)
+
+    return f"Showing {len(filtered_df):,.0f} records up to {selected_date}.", filter_condition
 
 @callback(
     Output("sales", "spec"),
-    Input("map", "signalData"),
+    Input("filter_condition", "data")
     # prevent_initial_call=True
 )
-def create_sales_chart(signal_data):
-    if not signal_data or not signal_data['selected_states']:
-        # If no state selected, use the entire data set
-        selection = df
-    else:
-        state = signal_data['selected_states']['state'][0]
-        selection = df[df['state'] == state]
+def create_sales_chart(query):
+    selection = df.query(query)
+    print(query)
+    print(len(selection))
 
     sales = alt.Chart(selection, width='container').mark_line().encode(
                 x=alt.X('yearmonth(Date):T', title='Month'),
@@ -388,16 +393,12 @@ def create_sales_chart(signal_data):
 
 @callback(
     Output("product", "spec"),
-    Input("map", "signalData"),
+    Input("filter_condition", "data")
     # prevent_initial_call=True
 )
-def create_product_chart(signal_data):
-    if not signal_data or not signal_data['selected_states']:
-        # If no state selected, use the entire data set
-        selection = df.groupby('Category')['Amount'].sum().reset_index()
-    else:
-        state = signal_data['selected_states']['state'][0]
-        selection = df[df['state'] == state].groupby('Category')['Amount'].sum().reset_index()
+def create_product_chart(query):
+    selection = df.query(query)
+    selection = selection.groupby('Category')['Amount'].sum().reset_index()
     
     product = alt.Chart(selection, width='container').mark_arc(innerRadius=50).encode(
                 theta="Amount",
