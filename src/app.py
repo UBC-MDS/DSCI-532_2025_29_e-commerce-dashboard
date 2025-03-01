51ac7aea3ed4da9ebba3aa2087c4a1dc7390f9d4
JENSON Chang <jensonchang@gmail.com>
2025-02-28 22:16:02 -0800

    select via dropdown

--- src/app.py
+++ src/app.py
@@ -10,41 +10,67 @@ alt.data_transformers.enable('vegafusion')
 url = 'https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_1_states_provinces.zip'
 world_regions = gpd.read_file(url)[['wikipedia', 'name', 'admin', 'postal', 'latitude', 'longitude', 'geometry']]
 
+def import_data():
+    df = pd.read_csv('data/raw/amazon_sample.zip', nrows=1000)
+    df = df.iloc[:, :-1]  # Drop last column
+    df.rename(columns={'ship-state' : 'state'}, inplace=True)
+    df['state'] = df['state'].str.title()
+    df['state'].dropna(inplace=True)
+    df['Date'] = pd.to_datetime(df['Date'])
+
+    url = 'https://naciscdn.org/naturalearth/50m/cultural/ne_50m_admin_1_states_provinces.zip'
+    india = gpd.read_file(url).query("iso_a2 == 'IN'")
+    india.rename(columns={'name' : 'state'}, inplace=True)
+
+    state_mapping = {
+        'Dadra And Nagar': 'Dadra and Nagar Haveli and Daman and Diu',
+        'New Delhi': 'Delhi',
+        'Andaman & Nicobar ': 'Andaman and Nicobar',
+        'Jammu & Kashmir ': 'Jammu and Kashmir',
+        'Rj': 'Rajasthan'
+    }
+
+    # Rename values in df
+    df['state'] = df['state'].replace(state_mapping)
+
+    # ship_states = set(df['state'].unique())
+    # india_states = set(india['state'].unique())
+    # missing_in_india = ship_states - india_states
+    # missing_in_ship = india_states - ship_states
+
+    # print("States in df but not in india:", missing_in_india)
+    # print("States in india but not in df:", missing_in_ship)
+
+    print(df.columns)
+    return df, india
+
+df, india = import_data()
+
 # Initiatlize the app
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
 # Layout
 app.layout = dbc.Container([
-    dcc.Dropdown(id='country', value='India', options=world_regions['admin'].unique()),
-    dvc.Vega(id='map', spec={}, signalsToObserve=['select_region']),
+    dcc.Dropdown(id='state', value='Delhi', options=df['state'].unique()),
+    dvc.Vega(id='sales', spec={}, signalsToObserve=['select_region']),
     # dcc.Markdown(id='chart-selection')
     html.Div(id='chart-selection')
 ])
 
 # Server side callbacks/reactivity
 @callback(
-    Output('map', 'spec'),
-    Input('country', 'value'),
+    Output('sales', 'spec'),
+    Input('state', 'value'),
 )
-def create_map(country):
-    select_region = alt.selection_point(fields=['name'], name='select_region')
-    return alt.Chart(world_regions.query(f'admin == "{country}"'), width=600, height=500).mark_geoshape(stroke='white').encode(
-        tooltip='name',
-        color=alt.Color('name').scale(scheme='tableau20'),  # To avoid repeating colors
-        opacity=alt.condition(select_region, alt.value(0.9), alt.value(0.3))
-    ).add_params(
-        select_region
-    ).to_dict(format="vega")
+def create_sales_chart(state):
+    selection = df[df['state'] == state]
+    sales = alt.Chart(selection, width='container').mark_line().encode(
+                x=alt.X('yearmonth(Date):T', title='Month'),
+                y=alt.Y('sum(Amount):Q', title='Total Amount')
+            ).to_dict(format='vega')
 
-@callback(
-    Output('chart-selection', 'children'),
-    Input('map', 'signalData'),
-)
-def print_selection(clicked_region):
-    print(clicked_region)  # So that you can see the full dictionary
-    if clicked_region and 'name' in clicked_region['select_region']:
-        return f'{clicked_region["select_region"]["name"]}'
+    return sales
 
 # Run the app/dashboard
 if __name__ == '__main__':
